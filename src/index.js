7b82dd160da93c309a9cd3201122fe60ba6cfeb7
Dan Hogue <chmod740@gmail.com>
2016-12-29 20:46:42 -0800

    change to hashhistory for easier s3 deploy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { browserHistory } from 'react-router'
+import { hashHistory, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import Root from './containers/Root'
 import configureStore from './store/configureStore'
 
 const store = configureStore()
-const history = syncHistoryWithStore(browserHistory, store)
 
-render(
-  <Root store={store} history={history} />,
-  document.getElementById('root')
+//---------------------------------------------
+// You can use either hashHistory or browserHistory here, but 
+// hashHistory works better for s3 type deployments.
+//---------------------------------------------
+const history = syncHistoryWithStore(hashHistory, store)
+
+render( <
+    Root store = { store }
+    history = { history }
+    />,
+    document.getElementById('root')
 )
