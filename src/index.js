b966976dda36278c54069755943a0001b96311d0
Dan Hogue <chmod740@gmail.com>
2016-12-31 23:42:51 -0800

    remove _k query from hashHistory, use indexRoute for postspage, and add about page shell code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { hashHistory, browserHistory } from 'react-router'
+import { hashHistory, browserHistory, useRouterHistory } from 'react-router'
+import { createHashHistory } from 'react-router/node_modules/history'
 import { syncHistoryWithStore } from 'react-router-redux'
 import Root from './containers/Root'
 import configureStore from './store/configureStore'
 import './styles/core.css'
 
+// This is hashHistory without the queryKey junk on the end of the url
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false })
+
 const store = configureStore()
 
-//---------------------------------------------
-// You can use either hashHistory or browserHistory here, but 
-// hashHistory works better for s3 type deployments.
-//---------------------------------------------
-const history = syncHistoryWithStore(hashHistory, store)
+// You can use hashHistory, appHistory or browserHistory here, but 
+// hashHistory/appHistory works better for s3 type deployments since
+// you don't have to hack a /* route.
+const history = syncHistoryWithStore(appHistory, store)
 
-render( < Root store = { store }
-    history = { history }
-    />,
+render(<Root store = { store } history = { history } />,
     document.getElementById('root')
 )
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Route } from 'react-router'
+import { Route, IndexRoute } from 'react-router'
 import Blog from './containers/Blog'
 import PostsPage from './containers/PostsPage'
 import PostPage from './containers/PostPage'
+import AboutPage from './containers/AboutPage'
 
-export default <Route path="/blog" component={Blog}>
-  <Route path="/"
-         component={PostsPage} />
-  <Route path="/posts/:post"
-         component={PostPage} />
+export default <Route path="/" component={Blog}>
+  <IndexRoute component={PostsPage} />
+  <Route path="/posts/:post" component={PostPage} />
+  <Route path='/about' component={AboutPage} />
 </Route>
